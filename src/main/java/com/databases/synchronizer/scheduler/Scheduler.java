c32412d4bffa9e68cf8e3334499b65e6bdb8a225
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-04-23 20:08:02 +0100

    Add logger for the Scheduler and the Synchronizer

--- src/main/java/com/databases/synchronizer/scheduler/Scheduler.java
+++ src/main/java/com/databases/synchronizer/scheduler/Scheduler.java
@@ -1,6 +1,5 @@
 package com.databases.synchronizer.scheduler;
 
-import com.databases.synchronizer.entity.Person;
 import com.databases.synchronizer.synchronization.Synchronizer;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +19,17 @@ public class Scheduler {
 
     private final ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
 
-    public void schedule(String table, Class clazz){
-
-         service.scheduleAtFixedRate(() -> {
-             try {
-                 synchronizer.synchronize(table, clazz);
-             } catch (Exception e){
-                 LOGGER.error("Error when trying to synchronize data :"+e);
-             }
-         }, 0, 1, TimeUnit.SECONDS);
+    public void schedule(String table, Class clazz) {
+        try {
+            service.scheduleAtFixedRate(() -> {
+                try {
+                    synchronizer.synchronize(table, clazz);
+                } catch (Exception e) {
+                    LOGGER.error("Error when trying to synchronize data :" + e);
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            LOGGER.error("Error when trying to schedule the synchronization for the table '" + table + "' : " + e + " - " + e.getCause());
+        }
     }
 }
--- src/main/java/com/databases/synchronizer/synchronization/Synchronizer.java
+++ src/main/java/com/databases/synchronizer/synchronization/Synchronizer.java
@@ -22,63 +22,66 @@ public class Synchronizer<T> {
     public Boolean synchronize(String table, Class<T> clazz) {
 
         long scrollTime = 60000;
+        try {
+            if (repository.findAll(clazz, scrollTime).size() == repository.getAll(table, clazz).size()) {
+                System.out.println("You have the same number of records { es : " + repository.findAll(clazz, scrollTime).size() +
+                        ", cass : " + repository.getAll(table, clazz).size() + " }");
+                return true;
+            } else if (repository.findAll(clazz, scrollTime).size() > repository.getAll(table, clazz).size()) {
+                List<T> fromElasticsearch = repository.findAll(clazz, scrollTime);
+                List<T> fromCassandra = repository.getAll(table, clazz);
+                Set<Object> inconsistentResults = new HashSet<>();
 
-        if (repository.findAll(clazz, scrollTime).size() == repository.getAll(table, clazz).size()) {
-            System.out.println("You have the same number of records { es : " + repository.findAll(clazz, scrollTime).size() +
-                    ", cass : " + repository.getAll(table, clazz).size() + " }");
-            return true;
-        } else if (repository.findAll(clazz, scrollTime).size() > repository.getAll(table, clazz).size()) {
-            List<T> fromElasticsearch = repository.findAll(clazz, scrollTime);
-            List<T> fromCassandra = repository.getAll(table, clazz);
-            Set<Object> inconsistentResults = new HashSet<>();
+                // TODO : This is just a note : We will consider Cassandra as a first
+                // TODO                data storage, so the number of documents in Elasticsearch
+                // TODO                will adjusted compared to the number of rows in Cassandra
 
-            // TODO : This is just a note : We will consider Cassandra as a first
-            // TODO                data storage, so the number of documents in Elasticsearch
-            // TODO                will adjusted compared to the number of rows in Cassandra
+                List<Object> idsFromElasticsearch = new ArrayList<>();
+                List<Object> idsFromCassandra = new ArrayList<>();
 
-            List<Object> idsFromElasticsearch = new ArrayList<>();
-            List<Object> idsFromCassandra = new ArrayList<>();
+                fromElasticsearch.forEach(t -> {
+                    try {
+                        Field field1 = t.getClass().getDeclaredField("id");
+                        field1.setAccessible(true);
+                        Object value1 = field1.get(t);
+                        idsFromElasticsearch.add(value1);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }
+                });
+                fromCassandra.forEach(p -> {
+                    try {
+                        Field field2 = p.getClass().getDeclaredField("id");
+                        field2.setAccessible(true);
+                        Object value2 = field2.get(p);
+                        idsFromCassandra.add(value2);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }
+                });
 
-            fromElasticsearch.forEach(t -> {
-                try {
-                    Field field1 = t.getClass().getDeclaredField("id");
-                    field1.setAccessible(true);
-                    Object value1 = field1.get(t);
-                    idsFromElasticsearch.add(value1);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                }
-            });
-            fromCassandra.forEach(p -> {
-                try {
-                    Field field2 = p.getClass().getDeclaredField("id");
-                    field2.setAccessible(true);
-                    Object value2 = field2.get(p);
-                    idsFromCassandra.add(value2);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                }
-            });
+                idsFromElasticsearch.forEach(id -> {
+                    if (!idsFromCassandra.contains(id)) {
+                        inconsistentResults.add(id);
+                        repository.deleteFromElasticsearch(clazz, id);
+                    }
+                });
+            } else if (repository.getAll(table, clazz).size() > repository.findAll(clazz, scrollTime).size()) {
+                List<T> fromElasticsearch = repository.findAll(clazz, scrollTime);
+                List<T> fromCassandra = repository.getAll(table, clazz);
 
-            idsFromElasticsearch.forEach(id -> {
-                if (!idsFromCassandra.contains(id)) {
-                    inconsistentResults.add(id);
-                    repository.deleteFromElasticsearch(clazz, id);
-                }
-            });
-        } else if (repository.getAll(table, clazz).size() > repository.findAll(clazz, scrollTime).size()) {
-            List<T> fromElasticsearch = repository.findAll(clazz, scrollTime);
-            List<T> fromCassandra = repository.getAll(table, clazz);
-
-            fromCassandra.forEach(entity -> {
-                if (!fromElasticsearch.contains(entity)) {
-                    repository.insertInElasticsearch(entity);
-                }
-            });
+                fromCassandra.forEach(entity -> {
+                    if (!fromElasticsearch.contains(entity)) {
+                        repository.insertInElasticsearch(entity);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error when trying to synchronize the table '" + table + "' : " + e + " - " + e.getCause());
         }
         return false;
     }
